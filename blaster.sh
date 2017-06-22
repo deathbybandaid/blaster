e41a0f3e15d987560fea1905d096d2b2c2a28fe8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-22 13:33:14 -0400

    Update blaster.sh

--- blaster.sh
+++ blaster.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 This is a shell dnsblaster
 
-## Vars
-HOSTTOBLAST=$(whiptail --inputbox "What DNS SERVER?" 10 80 "" 3>&1 1>&2 2>&3)
-DOMAINLISTURL="https://raw.githubusercontent.com/deathbybandaid/piholeparser/master/parsedall/1111ALLPARSEDLISTS1111.txt"
-DOMAINLIST="/home/domainlist"
-
 ## Deps
-WHATITIS=wamerican
-WHATPACKAGE=wamerican
-timestamp=$(echo `date`)
-if
-which $WHATITIS >/dev/null;
-then
-echo ""
-printf "$yellow"  "$WHATITIS is installed"
-echo "* $WHATITIS already installed $timestamp" | tee --append $RECENTRUN &>/dev/null
-else
-printf "$yellow"  "Installing $WHATITIS"
-apt-get install -y $WHATPACKAGE
-echo "* $WHATITIS was installed $timestamp" | tee --append $RECENTRUN &>/dev/null
-fi
-
 WHATITIS=dnsutils
 WHATPACKAGE=dnsutils
 timestamp=$(echo `date`)
@@ -30,12 +10,28 @@ which $WHATITIS >/dev/null;
 then
 echo ""
 printf "$yellow"  "$WHATITIS is installed"
-echo "* $WHATITIS already installed $timestamp" | tee --append $RECENTRUN &>/dev/null
 else
 printf "$yellow"  "Installing $WHATITIS"
 apt-get install -y $WHATPACKAGE
-echo "* $WHATITIS was installed $timestamp" | tee --append $RECENTRUN &>/dev/null
 fi
+
+WHATITIS=whiptail
+WHATPACKAGE=whiptail
+timestamp=$(echo `date`)
+if
+which $WHATITIS >/dev/null;
+then
+echo ""
+printf "$yellow"  "$WHATITIS is installed"
+else
+printf "$yellow"  "Installing $WHATITIS"
+apt-get install -y $WHATPACKAGE
+fi
+
+## Vars
+HOSTTOBLAST=$(whiptail --inputbox "What DNS SERVER?" 10 80 "" 3>&1 1>&2 2>&3)
+DOMAINLISTURL="https://raw.githubusercontent.com/deathbybandaid/piholeparser/master/parsedall/1111ALLPARSEDLISTS1111.txt"
+DOMAINLIST="/home/domainlist"
 
 ## Get Domain List
 wget -q -O $DOMAINLIST $DOMAINLISTURL
